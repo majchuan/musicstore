a5d81f3e473d98697d1c7d44ff20d998286019f9
majincan <mark@JinchuandeMacBook-Air.local>
2022-11-14 11:25:10 -0500

    commit

--- Repos/GenericRepository.cs
+++ Repos/GenericRepository.cs
@@ -37,6 +37,7 @@ public class GenericRepository<T> : IGenericRepository<T>  where T: class
     }
 
     public void RemoveRange(IEnumerable<T> t)
+    
     {
         ac.Set<T>().RemoveRange(t);
     }
